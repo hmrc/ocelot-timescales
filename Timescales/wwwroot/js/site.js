2697794f67d28012f72f55868b8dbddd8d3c5b60
Paul Temperley <paul.temperley@digital.hmrc.gov.uk>
2018-12-10 07:41:28 +0000

    add person details modal to audit page

--- Timescales/wwwroot/js/site.js
+++ Timescales/wwwroot/js/site.js
@@ -1,1 +1,41 @@
-﻿// Write your JavaScript code.
+﻿function getOwnerData(ownerPid) {
+
+    var url = "../../ad/api/loggedinuser/" + ownerPid;
+ 
+    return $.get(url);
+}
+
+function showUserData(ownerDetails) {
+
+    if (ownerDetails === undefined) {
+        $("#modalPid").val("PID not found");
+        $("#modalName").val("Name not found");
+        $("#modalPhone").val("Phone not found");
+        $("#modalEmail").val("Email not found");
+    } else {
+        $("#modalPid").val(ownerDetails.pid);
+        $("#modalName").val(ownerDetails.name);
+        $("#modalPhone").val(ownerDetails.phoneNumber);
+        $("#modalEmail").val(ownerDetails.email);
+    }
+}
+
+$('#OwnerModal').on('show.bs.modal', function (event) {
+
+    $("#modalPid").val("");
+    $("#modalName").val("");
+    $("#modalPhone").val("");
+    $("#modalEmail").val("");
+
+    var button = $(event.relatedTarget);
+    var owner = button.data('whatever');
+
+    getOwnerData(owner)
+        .done(function (data) {
+            showUserData(data);
+        })
+        .fail(function () {
+            showUserData();
+        });
+});
+
