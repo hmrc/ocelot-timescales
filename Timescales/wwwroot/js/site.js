38607275b50bccee2c0e35e526d24144e1b3cdea
Paul Temperley <paul.temperley@digital.hmrc.gov.uk>
2018-12-20 12:30:34 +0000

    modal view of owners list

--- Timescales/wwwroot/js/site.js
+++ Timescales/wwwroot/js/site.js
@@ -27,10 +27,57 @@ function getOwnerData(ownerPid, page) {
     } else if (page === "Edit") {
         url = "../../../ad/api/loggedinuser/" + ownerPid;
     } else if (page === "Audit") {
-        url = "../../ad/api/loggedinuser/" + ownerPid;
-    }    
+        url = "../../../ad/api/loggedinuser/" + ownerPid;
+    } else if (page === "Details") {
+        url = "../../../ad/api/loggedinuser/" + ownerPid;
+    }     
         
     return $.get(url);
+}
+
+function addUserData(ownerDetails) {
+
+    if (ownerDetails === undefined) {
+        $("#body").append(
+            '<div class="card">' +
+                '<div class= "card-body">' +
+                    '<label class= "control-label" >PID</label>' +
+                    '<input id="modalPid" class="form-control" disabled value="PID not found"/>' +
+
+                    '<label class="control-label">Name</label>' +
+                    '<input id="modalName" class="form-control" disabled value="PID not found"/>' +
+
+                    '<label class="control-label">Phone</label>' +
+                    '<input id="modalPhone" class="form-control" disabled value="PID not found"/>' +
+
+                    '<label class="control-label">Email</label>' +
+                    '<input id="modalEmail" class="form-control" disabled value="PID not found"/>' +
+                '</div>' +
+            '</div>' +
+
+            '<br />'
+        ); 
+    } else {
+        $("#body").append(
+            '<div class="card">' +
+                '<div class= "card-body">' +
+                    '<label class= "control-label" >PID</label>' +
+                    '<input id="modalPid" class="form-control" disabled value="' + ownerDetails.pid + '"/>' +
+
+                    '<label class="control-label">Name</label>' +
+                    '<input id="modalName" class="form-control" disabled value="' + ownerDetails.name + '"/>' +
+
+                    '<label class="control-label">Phone</label>' +
+                    '<input id="modalPhone" class="form-control" disabled value="' + ownerDetails.phoneNumber + '"/>' +
+
+                    '<label class="control-label">Email</label>' +
+                    '<input id="modalEmail" class="form-control" disabled value="' + ownerDetails.email + '"/>' +
+                '</div>' +
+            '</div>' +
+
+            '<br />'
+        ); 
+    }
 }
 
 function showUserData(ownerDetails) {
@@ -66,4 +113,24 @@ $('#OwnerModal').on('show.bs.modal', function (event) {
         .fail(function () {
             showUserData();
         });
+});
+
+$('#OwnerModalList').on('show.bs.modal', function (event) {
+
+    $("#body").empty();
+
+    var button = $(event.relatedTarget);
+    var owners = button.data('whatever');
+    var page = button.data('page');
+
+    owners.split(",").forEach(function (pid) {
+
+        $.when(getOwnerData(pid, page))
+            .done(function (data) {
+                addUserData(data);
+            })
+            .fail(function () {
+                addUserData();
+            });
+    });  
 });
